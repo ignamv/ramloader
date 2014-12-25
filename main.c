5bc4a3b802e1b937950a891080d49468de3f7b1c
Ignacio Martinez Vazquez <ignamv@gmail.com>
2014-12-25 10:55:26 -0300

    Clarify jump to program in main()

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 // Jump to a program stored in RAM
 
+// The linker will point this to the section reserved for the program
+// Right now this section is just a program in flash, but in the future it'll
+// be a dynamically loaded program in SRAM
 extern void _program();
 
 int main(void)
